7987648b4df61f062ffeaef71bc6665c87de96fb
Viktor Kerkez <alefnula@gmail.com>
2013-07-29 12:15:33 +0200

    Docs argument

--- docs/conf.py
+++ docs/conf.py
@@ -246,3 +246,6 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+# autodoc configuration
+autoclass_content = 'both'
